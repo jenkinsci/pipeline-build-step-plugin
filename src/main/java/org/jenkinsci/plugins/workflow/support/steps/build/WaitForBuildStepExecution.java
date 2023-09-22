f60781a5c5664c8df7149b638f0838c37c41c9b0
Stuart Rowe <stuartrowe@email.com>
2023-09-22 16:34:47 -0700

    Implement WaitForBuildStepExecution#stop so aborts can be propagated from the waiting build to the downstream waited for builds

--- src/main/java/org/jenkinsci/plugins/workflow/support/steps/build/WaitForBuildStepExecution.java
+++ src/main/java/org/jenkinsci/plugins/workflow/support/steps/build/WaitForBuildStepExecution.java
@@ -3,16 +3,26 @@ package org.jenkinsci.plugins.workflow.support.steps.build;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.AbortException;
 import hudson.console.ModelHyperlinkNote;
+import hudson.model.Computer;
+import hudson.model.Executor;
+import hudson.model.Queue;
 import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepExecutionImpl;
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class WaitForBuildStepExecution extends AbstractStepExecutionImpl {
 
     private static final long serialVersionUID = 1L;
+
+    private static final Logger LOGGER = Logger.getLogger(WaitForBuildStepExecution.class.getName());
 
     private final transient WaitForBuildStep step;
 
@@ -53,6 +63,53 @@ public class WaitForBuildStepExecution extends AbstractStepExecutionImpl {
             }
             return true;
         }
+    }
+
+    @Override
+    public void stop(@NonNull Throwable cause) throws Exception {
+        StepContext context = getContext();
+        Jenkins jenkins = Jenkins.getInstanceOrNull();
+        if (jenkins == null) {
+            context.onFailure(cause);
+            return;
+        }
+
+        boolean interrupted = false;
+
+        // if there's any in-progress build already, abort that.
+        // when the build is actually aborted, WaitForBuildListener will take notice and report the failure,
+        // so this method shouldn't call getContext().onFailure()
+        for (Computer c : jenkins.getComputers()) {
+            for (Executor e : c.getExecutors()) {
+                interrupted |= maybeInterrupt(e, cause, context);
+            }
+            for (Executor e : c.getOneOffExecutors()) {
+                interrupted |= maybeInterrupt(e, cause, context);
+            }
+        }
+
+        if (!interrupted) {
+            super.stop(cause);
+        }
+    }
+
+    private static boolean maybeInterrupt(Executor e, Throwable cause, StepContext context) {
+        boolean interrupted = false;
+        Queue.Executable exec = e.getCurrentExecutable();
+        if (exec instanceof Run) {
+            for(WaitForBuildAction waitForBuildAction : ((Run<?, ?>) exec).getActions(WaitForBuildAction.class)) {
+                if (waitForBuildAction.context.equals(context)) {
+                    e.interrupt(Result.ABORTED, new BuildTriggerCancelledCause(cause));
+                    try {
+                        ((Run<?, ?>) exec).save();
+                    } catch (IOException x) {
+                        LOGGER.log(Level.WARNING, "failed to save interrupt cause on " + exec, x);
+                    }
+                    interrupted = true;
+                }
+            }
+        }
+        return interrupted;
     }
 
 }
