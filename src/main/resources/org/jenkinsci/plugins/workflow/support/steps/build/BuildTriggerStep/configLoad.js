1fd04a283d021e5c735e201bc22825d0df53dd9a
Neetigya Poddar <neetigyapoddar1@gmail.com>
2023-10-10 08:28:23 +0530

    feat:Remove inline javascript code from BuildTriggerStep

--- src/main/resources/org/jenkinsci/plugins/workflow/support/steps/build/BuildTriggerStep/configLoad.js
+++ src/main/resources/org/jenkinsci/plugins/workflow/support/steps/build/BuildTriggerStep/configLoad.js
@@ -1,6 +1,10 @@
-function loadParams() {
-    var div = document.getElementById('params');
-    fetch('${descriptor.descriptorUrl}/parameters?job=' + encodeURIComponent(document.getElementById('${jobFieldId}').value) + '&amp;context=${descriptor.contextEncoded}').then((rsp) => {
+Behaviour.specify(".textbox-params-reference-holder", 'textbox-onblur-function', 0, function (e) {
+    var id = e.getAttribute('data-id');
+    var textbox = document.getElementById(id);
+
+    textbox.onblur = function(el) {
+        var div = document.getElementById('params');
+    fetch(document.querySelector('#params').dataset.descriptor + '/parameters?job=' + encodeURIComponent(document.getElementById(document.querySelector('#params').dataset.jobfield).value) + '&amp;context=${descriptor.contextEncoded}').then((rsp) => {
         rsp.text().then((responseText) => {
             if (rsp.ok) {
                 div.innerHTML = responseText;
@@ -10,4 +14,5 @@ function loadParams() {
             }
         });
     });
-}
+    }
+});
